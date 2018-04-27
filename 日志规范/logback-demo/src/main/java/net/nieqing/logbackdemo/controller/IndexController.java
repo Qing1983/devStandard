ad1076a055c05849f29b9422859c495aeea9e8f1
Xingkong <27447985@qq.com>
2018-04-28 01:12:18 +0800

    添加日志规范图片

--- 日志规范/logback-demo/src/main/java/net/nieqing/logbackdemo/controller/IndexController.java
+++ 日志规范/logback-demo/src/main/java/net/nieqing/logbackdemo/controller/IndexController.java
@@ -4,14 +4,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import net.nieqing.logbackdemo.vo.ResponseVo;
 
+@Slf4j
 @RestController
 @CrossOrigin
 public class IndexController {
 
 	@RequestMapping(value = "/index")
 	public ResponseVo index() {
+		log.info("[info] visit index");
+		log.debug("[debug] visit index");
+		log.error("[error] visit index");
+		log.warn("[warn] visit index");
 		return ResponseVo.success();
 	}
 
